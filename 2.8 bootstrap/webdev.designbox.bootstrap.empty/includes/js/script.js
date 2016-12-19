efcc150120b88ef6ed085f95dcee1c6c75b9aad6
cristinabotez <cristina_botez2003@yahoo.com>
2016-12-19 17:18:11 +0200

    Add files via upload

---
+++ 2.8 bootstrap/webdev.designbox.bootstrap.empty/includes/js/script.js
@@ -0,0 +1,18 @@
+/*
+
+DesignBox JS
+============
+
+
+
+*/
+
+$(function(){
+    $('.carousel').carousel({
+        interval: 2000
+    });
+});
+
+$(function(){
+    $('[rel="tooltip"]').tooltip();
+});
